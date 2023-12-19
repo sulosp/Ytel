d91c36efa52aa0c8a10449261e2c2d9c92e7184c
Sulochana P <sulochanasp@hotmail.com>
2023-12-19 07:51:14 +0530

    userLogin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,48 @@
+/** @type {import('tailwindcss').Config} */
 
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            cyan: '#5590F5',
-            blue:'#44BFFF',
+module.exports = {
 
-          }
-         
-        }
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
 
-        
-        
+  theme: {
+    colors: {
+      'blue': '#5590F5',
+      'cyan': '#44BFFF',
+      
+
+    },
+
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+    },
+
+    extend: {
+      gridTemplateColumns: {
+        '14': 'repeat(14, minmax(0, 1fr))',
+
+      },
+
+      borderRadius: {
+        '1xl': '8px',
+      },
+      boxShadow:{
+        '1xl':'0px 10px 30px 0px rgba(29, 73, 113, 0.10)',
+      }      
+    },
+
+    corePlugins: {
+      placeholderColor:{
+        grey:'#687175;',
+      },
+
+      input:{
+        color:'#000',
       }
     }
+
+    // ...
+  }
+}
