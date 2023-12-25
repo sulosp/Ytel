c4004737430c1ec8d93e49eb40cd98ee367a4427
sulosp <59638018+sulosp@users.noreply.github.com>
2023-12-26 00:03:44 +0530

    sidebar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend :{
+        colors: {
+         blue: '#5590F5',
+         cyan:'#44BFFF',
+        },
+
+        fontFamily:{
+          sans:['Open Sans', 'sans-serif'],
+        }
+      }
+    }
+  }
