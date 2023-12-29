40983436574bb73c3fca9b13b304e264c811ebb4
sulosp <59638018+sulosp@users.noreply.github.com>
2023-12-29 08:51:28 +0530

    v7

--- script.js
+++ script.js
@@ -34,6 +34,9 @@ searchInput.addEventListener('input', () => {
 
 
 // Disable right-click
-document.addEventListener('contextmenu', function (event) {
-  event.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+  // Disable right-click
+  document.addEventListener('contextmenu', function (event) {
+      event.preventDefault();
+  });
 });
